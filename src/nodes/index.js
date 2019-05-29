9b11afdeecf98cf1f20a5d3c8754f30562ba522a
Enrico Gigantiello <enrico.gigantiello@leanbakery.com>
2019-05-29 18:27:11 +0200

    custom publication info for metadata panel

--- src/nodes/index.js
+++ src/nodes/index.js
@@ -2,5 +2,6 @@ module.exports = {
   "cover": require("./cover"),
   "footnote": require("./footnote"),
   "footnote_reference": require("./footnote_reference"),
-  "citation": require("./citation")
+  "citation": require("./citation"),
+  "publication_info": require("./publication_info")
 };
