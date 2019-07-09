98b1a0d31f6f2a776010a9bb56bb9948f7ca0889
Enrico Gigantiello <enrico.gigantiello@leanbakery.com>
2019-07-09 17:10:33 +0200

    add specific parsers for link types

--- src/nodes/linkdata_service.js
+++ src/nodes/linkdata_service.js
@@ -7,15 +7,47 @@ LinkDataService.Prototype = function() {
   // Get all key references for a particular article
   // ---------------
   //
+  this.getBaseURLForType = {
+    "arachne": "https://arachne.dainst.org/data/entity/",
+    "gazetteer": "https://gazetteer.dainst.org/doc/"
+  }
 
-  this.getLinkData = function(slug, cb) {
+  this.parseArachneData = function(res) {
+    // return {
+    //   title: res.title,
+    //   subtitle: res.subtitle,
+    //   images: res.images,
+    // }
+    return res;
+  }
+  this.parseGazetteerData = function(res) {
+    // return {
+    //   provenance: res.provenance,
+    //   location: res.prefLocation,
+    //   prefName: res.prefName
+    // }
+    return res;
+  }
+
+  this.getLinkData = function(nodeProperties, cb) {
     // var doi = this.document.get('publication_info').doi;
-
+    var self = this;
+    var url = "";
+    url += this.getBaseURLForType[nodeProperties.urltype]
+    url += nodeProperties.slug
 		$.ajax({
-		  url: `https://arachne.dainst.org/data/entity/${slug}`,
+		  url: url,
 		  dataType: "json",
 		}).done(function(res) {
-			cb(null, res);
+      var parsedResponse = res;
+      if (nodeProperties.urltype === 'arachne') {
+        parsedResponse = self.parseArachneData(res)
+      }
+      if (nodeProperties.urltype === 'gazetteer') {
+        parsedResponse = self.parseGazetteerData(res)
+      }
+
+			cb(null, parsedResponse);
 		});
   };
 
