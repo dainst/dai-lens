9e0a892398aa6b277864e94c863fb3bbb3f03e93
Enrico Gigantiello <enrico.gigantiello@leanbakery.com>
2019-09-26 18:57:54 +0200

    fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,99 @@
-'use strict';
- 
-var gulp = require('gulp');
+const { src, dest, series, task } = require('gulp');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const babel = require('gulp-babel');
 var sass = require('gulp-sass');
-var browserify = require('browserify');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
+sass.compiler = require('node-sass');
 var through2 = require('through2');
-var path = require('path');
+var browserify = require('browserify');
+var concat = require('gulp-concat');
+const { rollup } = require('rollup');
 
-gulp.task('assets', function() {
-  gulp.src('assets/**/*', {base:"./assets"})
-        .pipe(gulp.dest('dist'));
+function assets(cb) {
+  src('assets/**/*', {base:"./assets"})
+    .pipe(dest('./dist'));
+  src('data/**/*', {base:"."})
+    .pipe(dest('./dist'));
+  cb()
+}
 
-  gulp.src('data/**/*', {base:"."})
-        .pipe(gulp.dest('dist'));
-});
-
-gulp.task('sass', function () {
-  gulp.src('./lens.scss')
+function styles(cb) {
+  return src('./lens.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(rename('lens.css'))
-    .pipe(gulp.dest('./dist'));
-});
+    .pipe(dest('./dist'));
+};
 
-gulp.task('browserify', function () {
-    return gulp.src('./boot.js')
-        .pipe(through2.obj(function (file, enc, next) {
-            browserify(file.path)
-                .bundle(function (err, res) {
-                    if (err) { return next(err); }
-                    file.contents = res;
-                    next(null, file);
-                });
-        }))
-        .on('error', function (error) {
-            console.log(error.stack);
-            this.emit('end');
-        })
-        .pipe(uglify())
-        .pipe(rename('lens.js'))
-        .pipe(gulp.dest('./dist'));
-});
 
-gulp.task('default', ['assets', 'sass', 'browserify']);
+
+function buildjs(cb){
+  return src('./boot.js')
+    .pipe(babel({
+        presets: ['@babel/env']
+    }))
+    .pipe(through2.obj(function (file, enc, next) {
+      console.log('browserify')
+        browserify(file.path)
+            .bundle(function (err, res) {
+                if (err) { 
+                  console.log('error here')
+                  return next(err); 
+                }
+                file.contents = res;
+                next(null, file);
+            });
+    }))
+    .on('error', function (error) {
+        console.log('aaaaaaaa', error)
+        console.log(error.stack);
+        this.emit('end');
+    })
+    .pipe(rename('lens.js'))
+    .pipe(uglify())
+    .pipe(dest('./dist'));
+}
+
+function build(cb) {
+  src('./boot.js')
+    .pipe(babel({
+        presets: ['@babel/env']
+    }))
+    // The gulp-uglify plugin won't update the filename
+    .pipe(uglify())
+    // So use gulp-rename to change the extension
+    .pipe(rename('lens.js'))
+    .pipe(dest('./dist'));
+  cb()
+}
+var paths = {
+  scripts: {
+    src: 'src/**/*.js',
+    dest: './dist'
+  }
+};
+
+
+function scripts() {
+  return src('./boot.js')
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(uglify())
+    .pipe(concat('lens.js'))
+    .pipe(dest(paths.scripts.dest));
+}
+
+
+// Rollup's promise API works great in an `async` task
+async function build_js() {
+  const bundle = await rollup({
+    input: './boot.js'
+  });
+
+  return bundle.write({
+    file: 'dist/lens.js',
+    format: 'iife'
+  });
+}
+
+exports.default = series(assets, styles, build_js);
